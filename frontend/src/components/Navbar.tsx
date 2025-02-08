f1b76eb116c56954f1ebbc27dbfedabe1a6fe334
sharvarihupare-369 <sharvarihupare15@gmail.com>
2025-02-08 16:41:28 +0530

    search input outline removed

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -72,7 +72,7 @@ const Navbar = ({
             type="text"
             value={inputVal}
             onChange={(e) => setInputVal(e.target.value)}
-            className="border border-gray-400 px-4 py-2 pr-10 rounded-md"
+            className="border border-gray-400 px-4 py-2 pr-10 rounded-md outline-none"
             placeholder="Search Tasks..."
           />
           <FaSearch className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" />
